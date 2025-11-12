bcdf71b22695dde9a55e52962e4a82c03008a7ef
John Doe <johndoe@example.com>
2025-11-12 18:57:50 +0700

    first commit

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    keyword = "man"
+    num_books_keyword = Book.objects.filter(title__icontains=keyword).count()
+
+    keywordforgenre = "fan"
+    num_genre_keywordforgenre = Genre.objects.filter(name__icontains=keywordforgenre).count()
+
+    # Генерация "количеств" некоторых главных объектов
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,
+                 "num_books_keyword" : num_books_keyword, "keyword":keyword,
+                 "num_genre_keywordforgenre" : num_genre_keywordforgenre, "num_genre_keywordforgenre":num_genre_keywordforgenre},
+    )
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
